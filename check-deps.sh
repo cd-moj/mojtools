4b24d68c6ce20a1d3f1a7a619bd362a2e1d6082e
Bruno Cesar Ribas <brunoribas@gmail.com>
2019-05-18 00:09:15 -0300

    check-deps.sh: Simple dep check to deploy elsewhere
    Signed-off-by: Bruno Cesar Ribas <brunoribas@gmail.com>

---
+++ check-deps.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HARDDEPS='bash awk getopt /usr/bin/time timeout bwrap diff make bc'
+
+SOFTDEPS='cset'
+
+COMPILERS='gcc g++ fpc javac gccgo python2 python3 bash spim'
+
+function checkdeps()
+{
+  local err=0
+  for arg; do
+    if ! which $arg &> /dev/null; then
+      echo "$arg"
+      ((err++))
+    fi
+  done
+  return $err
+
+}
+
+echo "Checking HARDDEPS"
+checkdeps $HARDDEPS && echo 'OK.'
+
+echo
+echo 'Checking SOFTDEPS'
+checkdeps $SOFTDEPS && echo 'OK.'
+
+echo
+echo 'Checking Compilers/runtime'
+checkdeps $COMPILERS && echo 'OK.'
