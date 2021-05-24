0b0273418c1269e6599e7a889c35ea945db3e34a
Raphael H Ribas <raphael.ribas@gmail.com>
2021-05-24 19:47:43 -0400

    rust support (#3)

--- check-deps.sh
+++ check-deps.sh
@@ -4,7 +4,7 @@ HARDDEPS='bash awk getopt /usr/bin/time timeout bwrap diff make bc'
 
 SOFTDEPS='cset'
 
-COMPILERS='gcc g++ fpc javac gccgo python2 python3 bash spim ocamlopt'
+COMPILERS='gcc g++ fpc javac gccgo python2 python3 bash spim ocamlopt rustc'
 
 function checkdeps()
 {
