119e15b0802ae20da8d34a6a039eebcf5f22847f
Bruno Ribas <ribas@naquadah.com.br>
2025-04-23 21:52:17 -0300

    lang/riscv/prep.sh: Add bindings for java
    This is necessary because the interpreter is in java and java need
    some security config that is stored at /etc
    
    Reported-by: Tiago Alves <fonsecafga@unb.br>
    Signed-off-by: Bruno Ribas <ribas@naquadah.com.br>

--- lang/riscv/prep.sh
+++ lang/riscv/prep.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+[[ -e /etc/java-21-openjdk/ ]] && EXTRABINDINGS+="-b /etc/java-21-openjdk/"
+[[ -e /etc/java ]] && EXTRABINDINGS+=" -b /etc/java"
+
 [[ ! -e /tmp/rars.jar ]] && wget https://github.com/TheThirdOne/rars/releases/download/v1.5/rars1_5.jar -O /tmp/rars.jar
 cp /tmp/rars.jar $1/
