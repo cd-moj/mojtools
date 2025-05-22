2a8bb1079ee672402918f41057d3fd57050a438b
Bruno Ribas <ribas@naquadah.com.br>
2025-05-21 22:38:35 -0300

    lang/apl/run.sh: fix it to run without the use o X
    Signed-off-by: Bruno Ribas <ribas@naquadah.com.br>

--- lang/apl/run.sh
+++ lang/apl/run.sh
@@ -7,4 +7,4 @@ exec &>/tmp/stderrlog
 cd /tmp/dir
 source binfile.sh
 
-exec dyalogscript ./$BIN < /tmp/in > /tmp/out
+exec dyalog APLTRANS=/opt/mdyalog/19.0/64/unicode/apltrans APLKEYS=/opt/mdyalog/19.0/64/unicode/aplkeys -s -script ./$BIN < /tmp/in > /tmp/out
